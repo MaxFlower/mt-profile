3e0b833faa8605bde27962fd718fd346dc0ac253
maxim.tsvetkov <maxtsvetapple@gmail.com>
2019-11-29 23:37:35 +0200

    feat: create app context to use it globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './App.module.scss';
 import { Experience, Header, Projects, Skills } from './components';
+import { AppContext, DEFAULT_APP_CONTEXT } from './AppContext';
 
 const App: React.FC = () => {
   return (
-    <div className={styles.App}>
-      <Header />
-      <Experience />
-      <Projects />
-      <Skills />
-    </div>
+    <AppContext.Provider value={DEFAULT_APP_CONTEXT}>
+      <div className={styles.App}>
+        <Header />
+        <Experience />
+        <Projects />
+        <Skills />
+      </div>
+    </AppContext.Provider>
   );
 };
 
