33fb235e2dd558e07a16e19ae8ab106ca34e49b6
maxim.tsvetkov <maxtsvetapple@gmail.com>
2020-09-09 10:55:29 +0300

    feat: use fetch method to get json data

---
+++ src/context/AppContext.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Label, LANGUAGE, PersonData, Skill } from '../definitions/data.interfaces';
+import { STATIC_DATA, STATIC_DATA_RU } from '../assets/data';
+
+export interface AppContext {
+  personalData: { [language: string]: PersonData | undefined };
+  currentLanguage: LANGUAGE;
+  skills: Skill[];
+  labels: { [language: string]: Label };
+}
+
+export const DEFAULT_APP_CONTEXT: AppContext = {
+  personalData: {},
+  currentLanguage: LANGUAGE.EN,
+  skills: [],
+  labels: {
+    'ru': STATIC_DATA_RU,
+    'en': STATIC_DATA,
+  },
+};
+
+export const AppContext = React.createContext(DEFAULT_APP_CONTEXT);
